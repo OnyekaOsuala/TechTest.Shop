f3f7cea9fe49295badd0a96e7d0845ad22afee36
Onyeka Osuala <ask4onyeka@yahoo.com>
2021-11-06 20:24:45 +0000

    Add project files.

---
+++ TechTest.Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTest.Shop;
+using TechTest.Shop.Repository;
+
+namespace TechTest.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In a real world app ProductRepository should be injected using dependency injection
+            ShoppingCart shoppingCart = new ShoppingCart(new ProductRepository());
+            shoppingCart.AddToCart(new List<string> { "Apple", "Apple", "Orange", "Apple" });
+
+            Console.WriteLine($"Total : {shoppingCart.Checkout()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TechTest.Shop/Models/IShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TechTest.Shop.Models
+{
+    public interface IShoppingCart
+    {
+        void AddToCart(List<string> items);
+
+        ICollection<CartItem> GetItems();
+
+        decimal GetTotalPrice(ICollection<CartItem> cartItems);
+
+        string Checkout();
+    }
+}
